54e0a4b8fb6476d745d996e2c6663df01ca96fef
mfanjaud <contact@marionfanjaud.fr>
2018-03-21 17:09:58 +0100

    update 21.03.1

---
+++ Leocha-IOS/Controller/RegisterViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RegisterViewController.swift
+//  Leocha-IOS
+//
+//  Created by Marion FANJAUD on 21/03/2018.
+//  Copyright © 2018 Marion FANJAUD. All rights reserved.
+//
+
+import UIKit
+
+
+class RegisterViewController: UIViewController {
+
+    
+    //Pre-linked IBOutlets
+
+    @IBOutlet var emailTextfield: UITextField!
+    @IBOutlet var passwordTextfield: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+  
+    @IBAction func registerPressed(_ sender: AnyObject) {
+        
+
+        
+        //TODO: Set up a new user on our Firbase database
+        
+        
+
+        
+        
+    } 
+    
+    
+}
---
+++ Leocha-IOS/Controller/WelcomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  WelcomeViewController.swift
+//  Leocha-IOS
+//
+//  Created by Marion FANJAUD on 21/03/2018.
+//  Copyright © 2018 Marion FANJAUD. All rights reserved.
+//
+
+import UIKit
+
+
+
+class WelcomeViewController: UIViewController {
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
